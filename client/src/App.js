b7e13b4bc7b744b249a71d4861954870ba941c4c
Leah Hullinger <leahhullinger@gmail.com>
2019-06-18 12:31:53 -0600

    header styled

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Header } from "./component/Header";
+import { Header } from "./component/Header/Header";
 import "./App.css";
+import keyboardIMG from "./keyboard-498396_1920.jpg";
 
 class App extends Component {
   constructor(props) {
@@ -36,37 +37,43 @@ class App extends Component {
       <div className="App">
         <Header />
         <div className="content">
-          <div className="user-input-wrapper">
+          {/* <div className="description">
+            <h2>
+              wondering how your email will come across? Run a quick tone
+              analysis before you send it.{" "}
+            </h2>
+          </div> */}
+          <div className="inputWrapper">
             <textarea
               rows={10}
               className="user-input"
               onChange={e => this.setState({ userInput: e.target.value })}
             />
-            <div>
-              <button onClick={this.getToneResults}>get results</button>
-            </div>
+            <button className="btn" onClick={this.getToneResults}>
+              check your tone
+            </button>
           </div>
-
-          {!!this.state.toneSummary && (
-            <div>
-              <div>
-                <h1>Tone Summary:</h1>
-                <p>the text you submitted included these tones: </p>
-              </div>
-              {this.state.toneSummary.tones.map(tone => (
-                <div key={tone.tone_id}>{tone.tone_name}</div>
-              ))}
-              {this.state.sentenceTones.map(sentence => {
-                return (
-                  <div key={sentence.sentence_id}>
-                    <div>{sentence.text}</div>
-                    <div>{sentence.tones}</div>
-                  </div>
-                );
-              })}
-            </div>
-          )}
         </div>
+
+        {!!this.state.toneSummary && (
+          <div>
+            <div>
+              <h1>Tone Summary:</h1>
+              <p>the text you submitted included these tones: </p>
+            </div>
+            {this.state.toneSummary.tones.map(tone => (
+              <div key={tone.tone_id}>{tone.tone_name}</div>
+            ))}
+            {this.state.sentenceTones.map(sentence => {
+              return (
+                <div key={sentence.sentence_id}>
+                  <div>{sentence.text}</div>
+                  <div>{sentence.tones.tone_name}</div>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
---
+++ client/src/component/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import keyboardIMG from "../../keyboard-498396_1920.jpg";
+import styles from "./Header.module.css";
+
+export const Header = () => (
+  <div className={styles.header}>
+    <h1 className={styles.titleTone}>Tone</h1>
+    {/* <p className={styles.descriptTone}>
+      check the tone of your email before you send it.
+    </p> */}
+    <h1 className={styles.titleInkblot}>Personality</h1>
+    {/* <p className={styles.descriptInkblot}>
+      Get a personality assessment by submitting 150+ words you've written.
+    </p> */}
+  </div>
+);
