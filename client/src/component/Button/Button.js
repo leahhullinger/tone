98a3ac8955368ac1f42ebd2df90680893712fd98
Leah Hullinger <leahhullinger@gmail.com>
2019-06-28 13:36:50 -0600

    button styled

--- client/src/component/Button/Button.js
+++ client/src/component/Button/Button.js
@@ -1,36 +1,10 @@
 import React from "react";
 import styles from "./Button.module.css";
 
-const btnStyle = {
-  btnWrapper: {
-    display: "flex",
-    justifyContent: "center",
-    alignContent: "right",
-    padding: 20,
-    width: "100%",
-    height: 50,
-    backgroundColor: "transparent"
-  },
-  btn: {
-    height: "100%",
-    textDecoration: "none",
-    backgroundColor: "transparent",
-    borderSize: 3,
-    borderColor: "black",
-    borderRadius: 5,
-    fontSize: "24px",
-    fontWeight: 800,
-    // textTransform: "uppercase",
-    paddingTop: 10,
-    paddingLeft: 15,
-    paddingRight: 15,
-    paddingBottom: 10
-  }
-};
 export const Button = ({ btnText, onClick }) => {
   return (
-    <div style={btnStyle.btnWrapper}>
-      <button style={btnStyle.btn} onClick={onClick}>
+    <div className={styles.btnWrapper}>
+      <button className={styles.btn} onClick={onClick}>
         {btnText}
       </button>
     </div>
